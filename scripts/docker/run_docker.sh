45f756cb914218ad67e48a7c3b800e9de26f03b3
pytholic <rajahaseeb147@gmail.com>
2022-12-21 09:58:46 +0900

    readme and dockerfile

--- scripts/docker/run_docker.sh
+++ scripts/docker/run_docker.sh
@@ -3,10 +3,9 @@ DATA_PATH="/home/$(id --user --name)"
 
 xhost +local:
 docker run -it \
-    --rm \
+    --name deepfactors \
     --runtime=nvidia \
     -v ${DATA_PATH}:${DATA_PATH} \
     -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix/:/tmp/.X11-unix:ro \
     deepfactors
-xhost -local:
